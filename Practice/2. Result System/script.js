a1e5e188e70ad9c229c2477155f93894ab236254
Aryan Rana <117369011+aryanrana19@users.noreply.github.com>
2024-06-20 01:29:32 +0530

    js

--- Practice/2. Result System/script.js
+++ Practice/2. Result System/script.js
@@ -1,6 +1,42 @@
-const mathsMarks = document.querySelector(".maths")
-const scienceMarks = document.querySelector(".science")
-const sstMarks = document.querySelector(".sst")
-const hindiMarks = document.querySelector(".hindi")
-const englishMarks = document.querySelector(".english")
-const form 
+const form = document.getElementById("form")
+const maths = document.querySelector(".maths")
+const science = document.querySelector(".science")
+const hindi = document.querySelector(".hindi")
+const english = document.querySelector(".english")
+const sst = document.querySelector(".sst")
+
+// SHOW ERROR FUNCTION
+function showError(input, message){
+  const formControl = input.parentElement
+  formControl.className = "form-control error"
+  const small = formControl.querySelector("small")
+  small.innerText = message 
+}
+
+// SHOW SUCCESS FUNCTION
+function showSuccess(input){
+  const formControl = input.parentElement
+  formControl.className = "form-control success"
+}
+
+// CHECK REQUIRED FIELDS
+function checkRequiredFields(inputArr){
+  inputArr.forEach(function(input){
+    if(input.value == ''){
+      showError(input, `${getFieldName(input)} + marks required`)
+    }else{
+      showSuccess(input)
+    }
+  })
+}
+
+// GET FIELD NAME
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+// FORM SUBMIT
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+  checkRequired([maths,science,sst,hindi,english])
+})
