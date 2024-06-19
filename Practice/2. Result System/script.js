254cd76635f887365898aba0b98ff645abae23bf
Aryan Rana <117369011+aryanrana19@users.noreply.github.com>
2024-06-19 22:56:28 +0530

    result system

---
+++ Practice/2. Result System/script.js
@@ -0,0 +1,6 @@
+const mathsMarks = document.querySelector(".maths")
+const scienceMarks = document.querySelector(".science")
+const sstMarks = document.querySelector(".sst")
+const hindiMarks = document.querySelector(".hindi")
+const englishMarks = document.querySelector(".english")
+const form 
