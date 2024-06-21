e7f87f0e1f8baf23d525255f11a5508000f3406e
Aryan Rana <117369011+aryanrana19@users.noreply.github.com>
2024-06-21 11:50:15 +0530

    taskList complete

--- 4. Task List/app.js
+++ 4. Task List/app.js
@@ -19,35 +19,41 @@ const taskInput = document.querySelector('#task')
 //   filter.addEventListener('keyup', filterTasks)
 // }
 
-
+document.addEventListener("DOMContentLoaded", getTasks)
 form.addEventListener("submit", (addTask))
 taskList.addEventListener("click", removeTask)
 clearBtn.addEventListener("click", clearTasks)
 filter.addEventListener('keyup', filterTasks)
 
+
+
 // ADD TASK
 function addTask(e){
   if(taskInput.value === ''){
     alert("Add a Task")
+  }else{
+    // CREATE li Element
+    const li = document.createElement("li")
+    // ADD CLASS
+    li.className = 'collection-item'
+    // CREATE TEXT NODE AND APPEND TO li
+    li.appendChild(document.createTextNode(taskInput.value))
+    // CREATE NEW LINK Element
+    const link = document.createElement('a')
+    // ADD CLASS
+    link.className = 'delete-item secondary-content'
+    // ADD icon HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // APPEND LINK TO li
+    li.appendChild(link)
+    // APPEND li TO ul
+    taskList.appendChild(li)
+    // STORE IN LOCAL STORAGE
+    storeTaskInLocalStorage(taskInput.value)
+    // CLEAR INPUT
+    taskInput.value = ''
   }
-  // CREATE li Element
-  const li = document.createElement("li")
-  // ADD CLASS
-  li.className = 'collection-item'
-  // CREATE TEXT NODE AND APPEND TO li
-  li.appendChild(document.createTextNode(taskInput.value))
-  // CREATE NEW LINK Element
-  const link = document.createElement('a')
-  // ADD CLASS
-  link.className = 'delete-item secondary-content'
-  // ADD icon HTML
-  link.innerHTML = '<i class="fa fa-remove"></i>'
-  // APPEND LINK TO li
-  li.appendChild(link)
-  // APPEND li TO ul
-  taskList.appendChild(li)
-  // CLEAR INPUT
-  taskInput.value = ''
+  
   e.preventDefault()
 }
 
@@ -68,6 +74,9 @@ function clearTasks(){
   // while(taskList.firstChild){
   //   taskList.removeChild(taskList.firstChild)
   // }
+
+  // CLEAR FROM LS
+  clearTasksFromLocalStorage()
 }
 
 // // FILTER TASKS
@@ -82,4 +91,63 @@ function filterTasks(e){
       task.style.display = 'none'
     }
   })
+}
+
+// GET TASKS FROM LS
+function getTasks(){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.forEach(function(task){
+    // create li element
+    const li = document.createElement('li')
+    // ADD CLASS
+    li.className = 'collection-item'
+    // CREATE TEXT NODE AND APPEND TO li
+    li.appendChild(document.createTextNode(task))
+    // CREATE NEW LINK ELEMENT
+    const link = document.createElement('a')
+    // ADD CLASS
+    link.className = 'delete-item secondary-content'
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // APPEND THE LINK TO li
+    taskList.appendChild(li)
+  })
+}
+
+// STORE IN LS
+function storeTaskInLocalStorage(task){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.push(task)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+// REMOVE FROM LS
+function removeFromLocalStorage(taskItem){
+  let tasks
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.slice(index, 1)
+    }
+  })
+  localStorage.setItem('tasks', JSON.stringify('tasks'))
+}
+
+
+// CLEAR TASKS FROM LS
+function clearTasksFromLocalStorage(){
+  localStorage.clear()
 }
--- Practice/4. Task List/app.js
+++ Practice/4. Task List/app.js
@@ -1,22 +1,34 @@
 const form = document.querySelector(".form")
 const taskInput = document.querySelector(".task-input")
-const taskField = document.querySelector(".task-field")
+const taskList = document.querySelector(".tasks")
+
+
+
+form.addEventListener("submit", (addTask))
 
 function addTask(e){
   if(taskInput.value === ''){
-    alert("Add a Task")
+    alert('Add a Task')
+  }else{
+    // CREATE A NEW TASK FIELD div
+    const taskField = document.createElement("div")
+    // ADD ITS CLASS
+    taskField.className = "task-field"
+    // CREATE TASK NAME ELEMENET
+    const h3 = document.createElement("h3")
+    h3.textContent = taskInput.value
+    console.log(taskInput.value)
+    const a = document.createElement("a")
+    a.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+
+    // APPEND CHILD
+    taskField.appendChild(h3)
+    taskField.appendChild(a)
+    
+    // APPEND TASKFIELD IN FORM
+    // form.appendChild(taskField)
+    taskList.appendChild(taskField)
+
   }
-  // CREATE NEW ELEMENT
-  const h3 = document.createElement("h3")
-  h3.textContent = taskInput.value
-  console.log(taskInput.value)
-  const a = document.createElement("a")
-  a.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-  
-  // APPEND CHILD
-  taskField.appendChild(h3)
-  taskField.appendChild(a)
   e.preventDefault()
 }
-
-form.addEventListener("submit", (addTask))
