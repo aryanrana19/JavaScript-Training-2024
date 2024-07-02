14cbfaf4c3a64d98386d7646626a3ced815c4693
Aryan Rana <117369011+aryanrana19@users.noreply.github.com>
2024-07-02 13:57:25 +0530

    ch

---
+++ Practice/Countdown App/app.js
@@ -0,0 +1,8 @@
+const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
+
+document.addEventListener("DOMContentLoaded", function(){
+  const currentTime = new Date()
+  console.log(currentTime)
+  const timeOut = new Date()
+})
